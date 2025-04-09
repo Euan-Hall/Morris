0c9cf5c5d5476fade34c8510d027ce1671603c68
Euan <hall.t.euan@gmail.com>
2025-04-10 00:41:05 +0100

    Added a simple DOM Parser. Will add in HTML parser next for DOM parser. Tags to be supported: - h1 - h5 - p - div - head - body
    Might get to do tag balancing if i can be bothered

--- nodes.py
+++ nodes.py
@@ -1,11 +1,68 @@
 class Node:
-    def __init__(self):
-        self.childeren = []
+    """
+    Base class for DOM classes.
+
+    Variables:
+    Children
+    """
+    def __init__(self, parent=None):
+        self.parent = parent
+        self.children = []
+
+        # If this node has a parent and isn't present as a child in the list, add to list.
+        if self.parent:
+            if self not in self.parent.children:
+                self.parent.addChild(self)
+
+    def addChild(self, child):
+        self.children.append(child)
+
+    def generateDict(self):
+        child = []
+
+        # If this node has children, generate a dictionary
+        if self.children:
+            for c in self.children:
+                temp =c.generateDict()
+                child.append(temp)
+
+        return {"Type" : type(self), 
+                "Children" : child}
 
 class Text(Node):
-    def __init__(self, text):
+    def __init__(self, text, parent=None):
         self.text = text
+        super().__init__(parent)
 
 class Element(Node):
-    def __init__(self, ElementData):
-        self.ElementData = ElementData
+    """
+    Element class (reperesnets HTML element, i.e. P, DIV)
+
+    Variables:
+    tag_name -> Type String
+    attr -> Array of class Attributes; help(attr) for more.
+
+    Inherits class Node.
+    """
+    def __init__(self, tag_name, parent=None):
+        self.tag_name = tag_name
+        self.attr = [] # Hash of Attributes
+        super().__init__(parent)
+
+
+class Document(Node):
+    def __init__(self, url="localhost", parent=None):
+        self.url = url
+        super().__init__(parent)
+
+
+class Attr(Node):
+    def __init__(self, parent, localname=None, name=None, value="", element=None):
+        self.localname = localname 
+        self.name = name
+        self.value = str(value)
+        self.element = element
+        super().__init__(parent)
+
+
+
